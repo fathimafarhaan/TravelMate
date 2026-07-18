85ec59b04b14c9782d423dc417dca4d594b44864
Fathima Farhaan <fathimafarhaan@Fathimas-MacBook-Air.local>
2026-07-18 16:17:57 +0530

    Add Notes API

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask
 import db
 from routes.trips import trips_bp
 from routes.places import places_bp
+from routes.notes import notes_bp
 
 app = Flask(__name__)
 db.init_db()
 
 app.register_blueprint(trips_bp)
 app.register_blueprint(places_bp)
+app.register_blueprint(notes_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
